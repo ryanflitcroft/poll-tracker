c1e197457da1f29cc51749fde3e1af890d9fffbf
Ryan Flitcroft <ryanflitcroft@ymail.com>
2021-12-08 10:58:54 -0800

    relevant DOM items grabbed in app.js

--- app.js
+++ app.js
@@ -1,4 +1,38 @@
 // import functions and grab DOM elements
+const inputForm = document.getElementById('input-form');
+
+const currentPollQuestion = document.getElementById('current-poll-question');
+const currentOptionA = document.getElementById('current-optionA');
+const currentOptionB = document.getElementById('current-optionB');
+
+const decrementA = document.getElementById('decrementA');
+const incrementA = document.getElementById('incrementA');
+const decrementB = document.getElementById('decrementB');
+const incrementB = document.getElementById('incrementB');
+
+const resultA = document.getElementById('resultA');
+const resultB = document.getElementById('resultB');
+
+const closePollButton = document.getElementById('close-poll-button');
+
+const closedPollContainer = document.getElementById('closed-poll-container');
+
+
+console.log(
+inputForm,
+currentPollQuestion,
+currentOptionA,
+currentOptionB,
+decrementA,
+incrementA,
+decrementB,
+incrementB,
+resultA,
+resultB,
+closePollButton,
+closedPollContainer
+);
+
 
 // let state
 
